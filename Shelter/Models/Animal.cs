9bd223fccb5603dec420b01fb97f0be910482ca7
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-18 10:34:48 -0700

    Add Shelter project files
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

---
+++ Shelter/Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace Shelter
+{
+  public class Shelter
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string AdmitDate { get; set; }
+    public string Breed { get; set; }
+    
+  }
+}
